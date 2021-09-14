bf0738e491350b79c7c24fe6dd373e4cb0b44e4f
SuminBae97 <steavee@o.cnu.ac.kr>
2021-09-14 14:43:32 +0900

    9095 복습

--- BOJ/add_three.py
+++ BOJ/add_three.py
@@ -18,3 +18,21 @@ def solution(n):
 for i in range(n):
     val = int(sys.stdin.readline())
     print(solution(val))
+import sys
+test_case = int(sys.stdin.readline())
+
+for _ in range(test_case):
+
+    val = int(sys.stdin.readline())
+
+    dp = [0] * (12)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, 11):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[val])
+
+# 9/14 review 1
