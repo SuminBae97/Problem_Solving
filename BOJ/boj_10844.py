72fd8a2390c154aeb3f8c92047449197c2cafb4c
Sumin Bae <55087554+SuminBae97@users.noreply.github.com>
2021-11-28 20:53:42 +0900

    Add files via upload

--- BOJ/boj_10844.py
+++ BOJ/boj_10844.py
@@ -1,22 +1,23 @@
-import sys
-n= int(sys.stdin.readline())
+from pprint import pprint
+n = int(input())
 
-
-dp = [[0 for i in range(10)] for j in range(101)]
+dp = [[0]*(101) for _ in range(10)]
 
 for i in range(1,10):
-    dp[1][i]=1
-dp[2] = [1,1,2,2,2,2,2,2,2,1]
+    dp[i][1]=1
 
-for i in range(3,n+1):
-    for j in range(0,10):
-        if j==0:
-            dp[i][j] = dp[i-1][j+1]
-        elif j==9:
-            dp[i][j]=dp[i-1][j-1]
-
+for j in range(2,n+1):
+    for k in range(0,10):
+        if k==0:
+            dp[k][j] = dp[k+1][j-1]
+        elif k==9:
+            dp[k][j] = dp[k-1][j-1]
         else:
-            dp[i][j] = dp[i-1][j-1]+dp[i-1][j+1]
+            dp[k][j] = dp[k+1][j-1] + dp[k-1][j-1]
+sum = 0
+for a in range(10):
+    sum+=dp[a][n]
+print(sum%(10**9))
 
 
-print(sum(dp[n])%(10**9))
+
