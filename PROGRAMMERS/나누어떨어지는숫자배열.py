980d49479d791f72ee790162dcf71cee0041a216
Sumin Bae <55087554+SuminBae97@users.noreply.github.com>
2021-12-09 15:24:26 +0900

    Add files via upload

---
+++ PROGRAMMERS/나누어떨어지는숫자배열.py
@@ -0,0 +1,14 @@
+def solution(arr, divisor):
+    answer = []
+    
+    for val in arr:
+        if val%divisor==0:
+            answer.append(val)
+    
+    if len(answer)==0:
+        answer.append(-1)
+    
+    answer.sort()
+    
+    
+    return answer
