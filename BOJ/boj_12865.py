f962fd8d5c2da35db2b36105e048522d94411e77
Sumin Bae <55087554+SuminBae97@users.noreply.github.com>
2021-12-29 14:19:43 +0900

    Add files via upload

--- BOJ/boj_12865.py
+++ BOJ/boj_12865.py
@@ -1,21 +1,24 @@
+import sys
+n,m = map(int,sys.stdin.readline().split())
 
-n,k = map(int, input().split())
-bag = [[0,0]]
-dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+val =[0]
+weight =[0]
 
-for _ in range(n):
-    bag.append(list(map(int, input().split())))
+for i in range(n):
+	it,vals = map(int,sys.stdin.readline().split())
+	weight.append(it)
+	val.append(vals)
+
+
+dp = [[0]*(m+1) for _ in range(n+1)]
 
 for i in range(1,n+1):
-    for j in range(1,k+1):
-        weight = bag[i][0]
-        val  = bag[i][1]
-
-        if j<weight:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j-weight]+val , dp[i-1][j])
+	for j in range(1,m+1):
+		
+		if weight[i] <= j:
+			dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight[i]]+val[i])
+		else:
+			dp[i][j] = dp[i-1][j]
 
 
-print(dp[n][k])
-
+print(dp[n][m])			
