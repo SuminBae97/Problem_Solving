0af50f96aebf0566d9d5844c097dabe31b058bc9
Sumin Bae <55087554+SuminBae97@users.noreply.github.com>
2022-01-01 12:17:36 +0900

    Add files via upload

---
+++ BOJ/boj_9251.py
@@ -0,0 +1,18 @@
+import sys
+
+first = sys.stdin.readline()
+second = sys.stdin.readline()
+
+
+dp = [[0]*(len(second)+1) for _ in range(len(first)+1)]
+
+for i in range(1,len(first)+1):
+    for j in range(1,len(second)+1):
+        if first[i-1]==second[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+
+        else:
+            dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+
+
+print(dp[-2][-2])
