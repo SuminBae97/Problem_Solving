732a53254ea3e498246232685d90e40b9a6eedbb
Sumin Bae <55087554+SuminBae97@users.noreply.github.com>
2022-01-05 10:37:52 +0900

    Add files via upload

---
+++ failed/playground.py
@@ -0,0 +1,20 @@
+import sys
+n = int(sys.stdin.readline())
+
+seq = list(map(int,sys.stdin.readline().split(" ")))
+dp = [1]*(n+1)
+
+for i in range(1,n):
+	for j in range(i):
+		if seq[j]<seq[i]:
+			dp[i] = max(dp[j]+1,dp[i])
+			
+print(max(dp))			
+			
+			
+
+
+
+
+
+
